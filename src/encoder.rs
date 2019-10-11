9f8c10b5d3b769763db43bfd423e7fdb7a4a02ea
Raphaël Zumer <rzumer@tebako.net>
2019-10-11 15:18:46 -0400

    Disable CDEF and LRF for 4:4:4 input

--- src/encoder.rs
+++ src/encoder.rs
@@ -220,8 +220,8 @@ impl Sequence {
       enable_warped_motion: false,
       enable_superres: false,
       enable_cdef: config.speed_settings.cdef
-        && config.chroma_sampling != ChromaSampling::Cs422,
-      enable_restoration: config.chroma_sampling != ChromaSampling::Cs422,
+        && config.chroma_sampling == ChromaSampling::Cs420,
+      enable_restoration: config.chroma_sampling == ChromaSampling::Cs420,
       enable_large_lru: true,
       operating_points_cnt_minus_1: 0,
       operating_point_idc,
